dde24f6fb4922e504893c45be46e2ecdf30af9ab
Comedy112 <hendrik.nel@my.jcu.edu.au>
2016-08-07 17:54:50 +1000

    exercise 6
    a class that displays your favourite movie quote on the screen

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,9 @@
+package com.jetbrains;
+
+public class Main {
+/* task 5*/
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Say hello to my little friend");
+    }
+}
