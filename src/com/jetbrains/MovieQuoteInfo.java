14af4ba6f8659ac83b79cdbe19e7b2d18669c559
Comedy112 <hendrik.nel@my.jcu.edu.au>
2016-08-07 18:06:17 +1000

    exercise 7
    not sure what was happning with the string :/

---
+++ src/com/jetbrains/MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+package com.jetbrains;
+
+public class MovieQuoteInfo {
+/* task 5*/
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Say hello to my little friend,Scarface,Tony Montana,1983");
+    }
+}
